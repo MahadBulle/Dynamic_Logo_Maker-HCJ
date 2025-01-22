e204c64eb7cbef99f33c757a962a943b422ac77c
Mahad Bulle <mahadhussein012@gmail.com>
2025-01-22 21:37:56 +0300

    usefull changes

--- Chapters.js
+++ Chapters.js
@@ -15,26 +15,4 @@ document.addEventListener('DOMContentLoaded', () => {
     const answer = document.getElementById(`answer${chapterNumber}`);
     answer.classList.toggle('hidden');
   }
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const chaptersLink = document.getElementById('chaptersLink');
-//     const chaptersSection = document.getElementById('chapters');
-  
-//     // Prevent default link behavior and show Chapters section
-//     chaptersLink.addEventListener('click', (e) => {
-//       e.preventDefault(); // Prevent navigation to another page
-//       if (chaptersSection.classList.contains('hidden')) {
-//         // If hidden, show the section
-//         chaptersSection.classList.remove('hidden');
-//       }
-//       window.scrollTo({ top: chaptersSection.offsetTop, behavior: 'smooth' }); // Scroll to section
-//     });
-//   });
-  
-//   // Function to toggle answer visibility
-//   function toggleAnswer(chapterNumber) {
-//     const answer = document.getElementById(`answer${chapterNumber}`);
-//     answer.classList.toggle('hidden');
-//   }
   
